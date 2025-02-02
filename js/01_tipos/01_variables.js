2c49d734a27e72ead5ea2ad10564b1763c7bba2a
Luis Rios <102693836+dovbleR@users.noreply.github.com>
2025-02-02 16:58:42 -0400

    Add files via upload

---
+++ js/01_tipos/01_variables.js
@@ -0,0 +1,12 @@
+UpperCamelCase, camelCase, snake_case
+
+let nombre = "Hola World";
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+console.log (nombre);
+
+let apellido;
+
+apellido = "Mundo ola";
---
+++ js/01_tipos/04_tipo-dinamico.js
@@ -0,0 +1,11 @@
+let numero = 10;
+let nombre = 'Luis';
+let verdadero = true;
+let undef;
+let nulo = null;
+
+console.log (numero);
+console.log (nombre);
+console.log (verdadero);
+console.log (undef);
+console.log (nulo);
